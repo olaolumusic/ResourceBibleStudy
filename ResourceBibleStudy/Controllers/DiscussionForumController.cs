16118a7a5ebabbf027bfa163ad94836608d97c4f
Olaolu Ajose <olaolumusic@yahoo.com>
2018-06-05 11:46:19 +0100

    API implementations and Exception Filters
    API implementations and Exception Filters

---
+++ ResourceBibleStudy/Controllers/DiscussionForumController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResourceBibleStudy.Controllers
+{
+    public class DiscussionForumController : Controller
+    {
+        //
+        // GET: /DiscussionForum/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ ResourceBibleStudy/Filters/ApiExceptionFilter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http.Filters;
+using ResourceBibleStudy.Models;
+
+namespace ResourceBibleStudy.Filters
+{
+    public class ApiExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            context.Response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+            context.Response.Content = CreateApiResponse(context);
+        }
+
+        private ObjectContent<ApiErrorMessage> CreateApiResponse(HttpActionExecutedContext context)
+        {
+            var apiErrorMessage = new ApiErrorMessage
+            {
+                ErrorMessage = context.Exception.Message,
+                ErrorType = context.Exception.GetType().ToString()
+            };
+
+            if (context.Exception is AggregateException)
+            {
+                apiErrorMessage = new ApiErrorMessage
+                {
+                    ErrorMessage = context.Exception.InnerException.Message,
+                    ErrorType = string.Format("{0}, {1}", context.Exception.GetType(), context.Exception.InnerException.GetType())
+                };
+            }
+
+            return new ObjectContent<ApiErrorMessage>(apiErrorMessage, new JsonMediaTypeFormatter());
+        }
+    }
+}
