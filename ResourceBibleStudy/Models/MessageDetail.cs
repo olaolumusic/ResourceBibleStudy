3c7b99dd63f3c2304118a6d81a431f0386fb516b
Olaolu Ajose <olaolumusic@yahoo.com>
2018-06-05 13:35:00 +0100

    Discussion Forum Using SignalR
    Discussion Forum Using SignalR
    Web App Screen Shots

---
+++ ResourceBibleStudy/Models/MessageDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace ResourceBibleStudy.Models
+{
+    public class MessageDetail
+    { 
+        public string UserName { get; set; }  
+        public string UserImageUrl { get; set; } 
+        public string Message { get; set; } 
+        public string MessageDateTime { get; set; }
+
+    }
+}
---
+++ ResourceBibleStudy/Models/UserDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace ResourceBibleStudy.Models
+{
+    public class UserDetail
+    {
+        public string ConnectionId { get; set; }
+        public string UserName { get; set; } 
+        public string UserImageUrl { get; set; }
+    }
+}
